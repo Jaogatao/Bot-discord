43fe1049b58759c21d6d7dc509bea79a34400b81
João Antônio <105800868+Jaogatao@users.noreply.github.com>
2024-06-01 23:50:44 -0300

    "Bot-Discord"

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+
+const dotenv = require('dotenv')
+dotenv.config()
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+
+
+client.once(Events.ClientReady, c => {
+	console.log(`Pronto meu chefe(a)! Login iniciado como ${c.user.tag}`)
+});
+
+client.login(TOKEN)
+
